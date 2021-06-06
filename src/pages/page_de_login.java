250a729d410c7efb6ab66b6c8d30fbaa2da87a2d
taojun wang <807740079@qq.com>
2021-06-06 17:04:44 +0200

    update login Page

--- src/pages/page_de_login.java
+++ src/pages/page_de_login.java
@@ -1,8 +1,17 @@
 package pages;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
+import javax.swing.border.Border;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class page_de_login {
 
@@ -36,8 +45,55 @@ public class page_de_login {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 900, 650);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		Border blackline = BorderFactory.createLineBorder(Color.black); //border of login panel
+		
+		JPanel login = new JPanel();
+		login.setBounds(206, 188, 450, 220);
+		frame.getContentPane().add(login);
+		login.setLayout(null);
+		login.setBorder(blackline);
+		
+		
+		JLabel id = new JLabel("id");
+		id.setHorizontalAlignment(SwingConstants.CENTER);
+		id.setBounds(42, 79, 65, 15);
+		login.add(id);
+		
+		JLabel mot_de_passe = new JLabel("mot_de_passe");
+		mot_de_passe.setHorizontalAlignment(SwingConstants.CENTER);
+		mot_de_passe.setBounds(29, 125, 104, 15);
+		login.add(mot_de_passe);
+		
+		JLabel idientification = new JLabel("idientification");
+		idientification.setHorizontalAlignment(SwingConstants.CENTER);
+		idientification.setBounds(138, 25, 200, 30);
+		login.add(idientification);
+		
+		JTextPane input_id = new JTextPane();
+		input_id.setBounds(171, 79, 200, 25);
+		login.add(input_id);
+		
+		JTextPane input_mot = new JTextPane();
+		input_mot.setBounds(171, 125, 200, 25);
+		login.add(input_mot);
+		
+		JLabel oublier_mot_de_passe = new JLabel("oublier?");
+		oublier_mot_de_passe.setHorizontalAlignment(SwingConstants.CENTER);
+		oublier_mot_de_passe.setBounds(39, 150, 58, 15);
+		login.add(oublier_mot_de_passe);
+		
+		JButton connecter = new JButton("se connecter");
+		connecter.setBounds(138, 174, 120, 25);
+		login.add(connecter);
+		
+		JLabel title = new JLabel("DEPARTEMENT MANAGEMENT");
+		title.setHorizontalAlignment(SwingConstants.CENTER);
+		title.setFont(new Font("ËÎÌו", Font.BOLD, 12));
+		title.setBounds(222, 34, 430, 50);
+		frame.getContentPane().add(title);
 	}
-
 }
