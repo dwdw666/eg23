6bd901a4413a41406bfdc30a82f8d343b35d42de
dwdw666 <61399280+dwdw666@users.noreply.github.com>
2021-06-06 19:59:58 +0200

    page login

--- src/pages/page_de_login.java
+++ src/pages/page_de_login.java
@@ -9,13 +9,17 @@ import javax.swing.JLabel;
 import javax.swing.JTextPane;
 import javax.swing.border.Border;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import javax.swing.JTextField;
 
 public class page_de_login {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -58,23 +62,62 @@ public class page_de_login {
 		panel.setLayout(null);
 		
 		JPanel logIn = new JPanel();
-		logIn.setBounds(239, 192, 445, 213);
+		logIn.setBounds(202, 179, 498, 247);
+		logIn.setBackground(new Color(115, 175, 237));
 		panel.add(logIn);
 		logIn.setLayout(null);
 		
 		JLabel title_logIn = new JLabel("Identification");
 		title_logIn.setHorizontalAlignment(SwingConstants.CENTER);
-		title_logIn.setBounds(114, 24, 200, 30);
+		title_logIn.setBounds(114, 10, 250, 48);
 		title_logIn.setBorder(blackline);
+		title_logIn.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 30));
+		title_logIn.setForeground(new Color(21, 50, 207));
 		logIn.add(title_logIn);
+		
+		JLabel id = new JLabel("Id:");
+		id.setHorizontalAlignment(SwingConstants.CENTER);
+		id.setBounds(30, 88, 64, 15);
+		id.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 15));
+		id.setForeground(new Color(21, 50, 207));
+		logIn.add(id);
+		
+		JLabel passeWord = new JLabel("Mot de passe : ");
+		passeWord.setBounds(10, 130, 111, 17);
+		passeWord.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 15));
+		passeWord.setForeground(new Color(21, 50, 207));
+		logIn.add(passeWord);
+		
+		textField = new JTextField();
+		textField.setBounds(147, 82, 262, 33);
+		logIn.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(147, 125, 262, 33);
+		logIn.add(textField_1);
+		
+		JLabel oublie_mot = new JLabel("Mot de passe oubli¨¦?");
+		oublie_mot.setBounds(10, 163, 128, 15);
+		logIn.add(oublie_mot);
+		
+		JButton connecter = new JButton("SE CONNECTER");
+		connecter.setBounds(173, 189, 143, 23);
+		logIn.add(connecter);
 		
 		JLabel title = new JLabel("DEPARTEMENT MANAGEMENT");
 		title.setHorizontalAlignment(SwingConstants.CENTER);
-		title.setBounds(243, 30, 430, 50);
+		title.setBounds(265, 35, 486, 50);
+		title.setBackground(new Color(115, 175, 237));
+		title.setOpaque(true);
 		title.setBorder(blackline);
+		title.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 30));
+		title.setForeground(new Color(21, 50, 207));
 		panel.add(title );
 		
 		JLabel uttLabel = new JLabel();
+		uttLabel.setIcon(new ImageIcon(getClass().getResource("/Image/UTT.png")));
 		uttLabel.setBounds(10, 10, 210, 90);
 		panel.add(uttLabel);
 	}
