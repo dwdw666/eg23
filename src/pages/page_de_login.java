600f2d1cca343330de53b28264df569b83257cb8
taojun wang <807740079@qq.com>
2021-06-06 09:45:38 +0200

    'first'

---
+++ src/pages/page_de_login.java
@@ -0,0 +1,43 @@
+package pages;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class page_de_login {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					page_de_login window = new page_de_login();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public page_de_login() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
